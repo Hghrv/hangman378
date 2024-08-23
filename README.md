154a070bc995405cffa7fcede9fc7f1ac9bfce0d
Hghrv <htchoumkeu@yahoo.fr>
2024-08-23 21:19:12 +0100

    Creates variables for the game after successfully setting up VSC-Gitbash environment

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 Hangman is a classic game in which a player thinks of a word and the other player tries to guess that word within a certain amount of attempts.
 
 This is an implementation of the Hangman game, where the computer thinks of a word and the user tries to guess it. 
+
+Project Title: Hangman
+Table of Contents:
+    1. Description
+    2. Instructions (Installation/Usages)
+    3. File structure
+    4. Licence information
+
+1. A description of the project
+What it does: 
+The aim of the project:
+Key points learned:
+2. Instructions
+2a. Installation instructions
+2b. Usage instructions
+3. File structure of the project
+4. License information
--- milestone_2.py
+++ milestone_2.py
@@ -1,6 +1,25 @@
+#Milestone 1. Environment setup: Git environment setup under Git-Bash terminal successful
+# Github repository created successfully
+# Github repo successfully cloned in local Git
+
+#Milestone 2. Creates the variables for the games
+#Imports the module Random
 import random
 
+#Prints a list of favourite fruits
 word_list = ["Lemon", "Mango", "Banana", "Orange", "Avocado"]
 print(word_list)
+
+#Prints a fruit name randomly from the list
 word = random.choice(word_list)
 print(word)
+
+#Requests input from user and assigns it to a string variable
+guess = input('Enter a single letter of your choice : _ ')
+
+#Checks that the input is a single alphabetical character
+if len(guess)== 1 and guess.isalpha() == True :
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
+
