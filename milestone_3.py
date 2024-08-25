f9c4026a68c3f499a9d00f4924719940c0e5514a
Hghrv <htchoumkeu@yahoo.fr>
2024-08-25 23:25:14 +0100

    Checks whether the guess in the word

--- milestone_3.py
+++ milestone_3.py
@@ -1,6 +1,13 @@
+#Iteratively checks that the input is a valid letter
 while True:
     guess = input("Guess a single letter of your choice : _ ")
     if len(guess) == 1 and guess.isalpha() == True:
         break
     else:
         print("Invalid letter. Please, enter a single alphabetical character.")
+
+#Check whether the guess in the word
+if guess is in word:
+    print(f"Good guess! {guess} is in the word.")
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
