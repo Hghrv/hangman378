6b1ceb4dc8b517cb4afc71a61cc0e4711548757c
Hghrv <htchoumkeu@yahoo.fr>
2024-08-28 02:28:07 +0100

    Defines what happens if the letter is in the word

--- milestone_4.py
+++ milestone_4.py
@@ -6,15 +6,18 @@ class Hangman:
         self.num_lives = num_lives
         word_list = ["lemon", "mango", "banana", "orange", "avocado"]
         word = word_list.random()
-        num_letters = len(word)
-        word_guessed = ['_' for i in range(0, num_letters) ]
-        list_of_guesses =[]
-
+        num_letters = len(set(word))
+        word_guessed = ['_' for element in range(0, num_letters)]
+       
     def check_guess(guess):
         guess.lower()
         if guess is in word:
             print(f"Good guess! {guess} is in the word.")
-            
+            for index in range(0, len(word)):
+                if word[index] == guess:
+                    word_guessed[index] = guess
+            num_letters = num_letters - 1
+
     def ask_for_input():
     #Iteratively checks that the input is a valid letter
         while True:
